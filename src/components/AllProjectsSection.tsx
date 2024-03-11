a81db02c28101ed078d148b80070d12f47747ce7
Parshuram <parshurambagade855@gmail.com>
2024-03-11 13:27:57 +0530

    changed .gif to webp

--- src/components/AllProjectsSection.tsx
+++ src/components/AllProjectsSection.tsx
@@ -12,7 +12,7 @@ const page = () => {
             <h2 className='text-4xl md:text-5xl text-center text-cyan-500 font-bold my-4'>All Projects</h2>
             <p className='text-center'>Here is a list of my all projects</p>
         </div>
-        <div className='md:grid lg:grid-cols-2 w-full h-max pb-8'>
+        <div className='md:grid lg:grid-cols-2 xl:grid-cols-3 w-full h-max pb-8'>
             {projectsData.projects.map((project) => (<ThreeDCard key={project.id} title={project.title} subtitle={project.subtitle} image={project.img} github={project.codeUrl} liveLink={project.liveUrl} desc={project.desc} techStack={project.techstack} />))}
         </div>
 

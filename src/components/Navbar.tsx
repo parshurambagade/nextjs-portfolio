98e59934282abf238b4922d42579c1b7c34d507f
Parshuram Bagade <parshurambagade855@gmail.com>
2025-07-08 11:10:51 +0530

    fix(header): add html symentic tag for header and replace z index from 50 to 20

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,64 @@
 "use client";
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/components/ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
 const Navbar = ({ className }: { className?: string }) => {
-    const [active, setActive] = useState<string | null>(null);
+  const [active, setActive] = useState<string | null>(null);
 
   return (
-    <div className={cn(" fixed top-8 max-h-max lg:bottom-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
-        
-        <Menu setActive={setActive}>
-            <Link href={"/"}>
-            <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
-            </Link>
-            <Link href={"/about"}>
-            <MenuItem setActive={setActive} active={active} item="About"></MenuItem>
-            </Link>
-            <Link href={"/#skills"}>
-              <MenuItem setActive={setActive} active={active} item="Skills"></MenuItem>
-            </Link>
-            <Link href={"/#projects"}>
-              <MenuItem setActive={setActive} active={active} item="Projects"></MenuItem>
-            </Link>
-            
+    <header
+      className={cn(
+        " fixed top-8 max-h-[64px] lg:bottom-10 inset-x-0 max-w-2xl mx-auto z-20",
+        className
+      )}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <Link href={"/about"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="About"
+          ></MenuItem>
+        </Link>
+        <Link href={"/#skills"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Skills"
+          ></MenuItem>
+        </Link>
+        <Link href={"/#projects"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Projects"
+          ></MenuItem>
+        </Link>
 
-            <Link href={"/#contact"}>
-            <MenuItem setActive={setActive} active={active} item="Contact"></MenuItem>
-            </Link>
-        </Menu>
-    </div>
-  )
-}
+        <Link href={"/#contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
